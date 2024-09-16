c9afce51fc1da68e0a655f43bf289afaa7f1f9d1
richard.liao <richard.liao@tpisoftware.com>
2024-09-16 20:30:22 +0800

    chore: expo-speech 無法發出聲音

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,17 +1,58 @@
-import {  StyleSheet,  } from "react-native";
+import { Button, Platform, StyleSheet, Text, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
-import { HelloWave } from "@/components/HelloWave";
-import { ThemedText } from "@/components/ThemedText";
-import { ThemedView } from "@/components/ThemedView";
+import { useEffect, useState } from "react";
+import * as Speech from "expo-speech";
+import { Audio } from "expo-av";
+
+const json: Json = require("@/assets/dictionary.json");
+export interface Json {
+  greWord: GreWord;
+}
+
+export interface GreWord {
+  meaning: string;
+  word: string;
+}
 
 export default function HomeScreen() {
+  const [permissionResponse, requestPermission] = Audio.usePermissions();
+
+  useEffect(() => {
+    enableSound();
+  }, []);
+
+  const enableSound = async () => {
+    const { sound } = await Audio.Sound.createAsync(
+      require("@/assets/soundFile.mp3")
+    );
+    if (Platform.OS === "ios") {
+      await Audio.setAudioModeAsync({
+        playsInSilentModeIOS: true,
+      });
+      await sound.playAsync();
+    }
+    let voices = await Speech.getAvailableVoicesAsync();
+    // console.log(voices);
+  };
+
+  const speak = () => {
+    console.log("speak");
+    const greeting = `123`;
+    const options = {
+      voice: "com.apple.voice.compact.zh-TW.Meijia",
+      pitch: 1.5,
+    };
+    Speech.speak(greeting, options);
+  };
+
   return (
     <>
       <SafeAreaView style={{ flex: 1 }}>
-        <ThemedView style={styles.titleContainer}>
-          <ThemedText type="title">Welcome!</ThemedText>
-          <HelloWave />
-        </ThemedView>
+        <Button title="Press to hear some words" onPress={speak} />
+        {/* <View>
+          <Text onPress={handlePress}>{json.greWord.word}</Text>
+          <Text>{json.greWord.meaning}</Text>
+        </View> */}
       </SafeAreaView>
     </>
   );
@@ -20,8 +61,8 @@ export default function HomeScreen() {
 const styles = StyleSheet.create({
   titleContainer: {
     flexDirection: "row",
-    flex:1,
-    justifyContent:"center",
+    flex: 1,
+    justifyContent: "center",
     alignItems: "center",
   },
 });
